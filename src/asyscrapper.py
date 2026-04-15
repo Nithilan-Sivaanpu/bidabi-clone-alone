a56ff8057ebbac69c7febd77201c800fa6ea253a
Nithilan-Sivaanpu <nithilan.sivaanpu@edu.univ-paris13.fr>
2026-04-15 11:42:34 +0200

    Add the data for several categories

--- src/asyscrapper.py
+++ src/asyscrapper.py
@@ -74,7 +74,10 @@ async def fetch_page(session, category, page, page_size, sem):
 # -------------------------
 # Async image download
 # -------------------------
-async def download_image(session, url, image_id, sem, folder="data/images/sugar"):
+async def download_image(session, url, image_id, sem, category,folder=None):
+    if folder is None:
+        folder = f"data/raw/images/{category}"
+
     if not url:
         return
 
@@ -126,7 +129,7 @@ async def scrape(category, target_count, page_size, max_pages):
                     image_id = info[0]
 
                     task = asyncio.create_task(
-                        download_image(session, image_url, image_id, sem_img)
+                        download_image(session, image_url, image_id, sem_img, category)
                     )
                     image_tasks.append(task)
 
@@ -154,7 +157,8 @@ def save_to_csv(filename, rows):
 # -------------------------
 def main():
     products = asyncio.run(scrape(CATEGORY, TARGET_COUNT, PAGE_SIZE, MAX_PAGES))
-    output_file = f"{OUTPUT_DIR}/metadata_{CATEGORY}_{TARGET_COUNT}.csv"
+    output_file = f"data/raw/images/{CATEGORY}/metadata_{CATEGORY}.csv"
+    os.makedirs(f"data/raw/images/{CATEGORY}", exist_ok=True)
     save_to_csv(output_file, products)
     print(f"✔ Fichier {output_file} créé. Produits valides collectés : {len(products)}")
 
